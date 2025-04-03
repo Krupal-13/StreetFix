0e5c957014bea7da433c82016b494ba763ea758c
KansaraKrish <krishkansara568@gmail.com>
2025-04-03 10:37:00 +0530

    new

---
+++ frontend/src/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# MongoDB Connection String
+app.config["MONGO_URI"] = "mongodb+srv://Krish:SMC4yGA1VzmtvZpd@cluster0.dsmrx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongo = PyMongo(app)
+users = mongo.db.users  # Collection name
+
+# Signup Route
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    if users.find_one({"username": username}):
+        return jsonify({"error": "Username already exists"}), 400
+
+    hashed_password = generate_password_hash(password)
+    users.insert_one({"username": username, "email": email, "password": hashed_password})
+    
+    return jsonify({"message": "User registered successfully!"}), 201
+
+# Login Route
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    user = users.find_one({"username": username})
+    if user and check_password_hash(user["password"], password):
+        return jsonify({"message": "Login successful!"}), 200
+    else:
+        return jsonify({"error": "Invalid credentials"}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
