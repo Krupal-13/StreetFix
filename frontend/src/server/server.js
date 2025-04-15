751296f830a19d8946f72bda9ab33d0ea00bed63
KansaraKrish <krishkansara568@gmail.com>
2025-04-15 07:49:17 +0530

    little nothing

--- frontend/src/server/server.js
+++ frontend/src/server/server.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/auth");
 
 dotenv.config({ path: "./src/server/config.env" });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 const app = express();
 app.use(cors());
